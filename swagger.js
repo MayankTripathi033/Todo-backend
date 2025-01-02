1791c9e9a3688e94144dd52a4e269fc52fe18649
MayankTripathiMIDAS <165718550+MayankTripathiMIDAS@users.noreply.github.com>
2025-01-02 22:37:49 +0530

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3001/api", // Base URL for API
+        url: "https://api.mayanktripathi.online/api", // Base URL for API
       },
     ],
     components: {
