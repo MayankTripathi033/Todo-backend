e481d58db70b79b423b03c43663dc29cd07cad3f
MayankTripathi033 <mayank033023@gmail.com>
2025-02-04 05:10:02 +0530

    Fixed CORS maybe

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(express.json()); //Converts JSON string to JSON object
 const corsOptions = {
   origin: "*", // Allow only specific frontend domain
   methods: ["GET", "POST", "DELETE", "PATCH"], // Allow only specific methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
 };
 app.use(cors(corsOptions));
+app.use(express.json()); //Converts JSON string to JSON object
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 const port = process.env.PORT || 3001;
 
